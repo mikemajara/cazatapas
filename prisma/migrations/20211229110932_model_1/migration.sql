3cf369661d1f636ff521d6578cf8bb83d5046611
mikemajara <fmiguel05@gmail.com>
2021-12-29 12:38:18 +0100

    chore: seed

---
+++ prisma/migrations/20211229110932_model_1/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Restaurant" ALTER COLUMN "url" DROP NOT NULL;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,6 +11,116 @@ const userData: Prisma.UserCreateInput[] = [
   },
 ];
 
+const tagData: Prisma.TagCreateInput[] = [
+  { id: "soy", name: "soy" },
+  { id: "fish", name: "fish" },
+  { id: "mustard", name: "mustard" },
+  { id: "mollusks", name: "mollusks" },
+  { id: "dairy", name: "dairy" },
+  { id: "eggs", name: "eggs" },
+  { id: "sesame", name: "sesame" },
+  { id: "gluten", name: "gluten" },
+  { id: "nuts", name: "nuts" },
+  { id: "dioxide", name: "sulfur dioxide" },
+  { id: "crustaceans", name: "crustaceans" },
+  { id: "peanuts", name: "peanuts" },
+  { id: "celery", name: "celery" },
+  { id: "lupins", name: "lupins" },
+];
+
+const restaurantData: Prisma.RestaurantCreateInput[] = [
+  {
+    name: "La Tapeoteca",
+    images: {
+      create: {
+        name: "la-tapeopteca",
+        location: "/restaurants/la-tapeoteca.png",
+      },
+    },
+  },
+  {
+    name: "Los Zagales",
+    images: {
+      create: {
+        name: "los-zagales",
+        location: "/restaurants/los-zagales.png",
+      },
+    },
+  },
+];
+
+const dishData: Prisma.DishCreateInput[] = [
+  {
+    name: "Marinera negra con gulas al ajillo",
+    price: 2.2,
+    restaurant: { connect: { name: "La Tapeoteca" } },
+    tags: {
+      connect: [
+        { id: "soy" },
+        { id: "fish" },
+        { id: "lupins" },
+        { id: "nuts" },
+        { id: "dioxide" },
+      ],
+    },
+  },
+  {
+    name: "Croqueta cremosa de jamón ibérico",
+    price: 2.2,
+    restaurant: { connect: { name: "La Tapeoteca" } },
+    tags: {
+      connect: [
+        { id: "dairy" },
+        { id: "lupins" },
+        { id: "gluten" },
+        { id: "dioxide" },
+      ],
+    },
+  },
+  {
+    name: "Pincho moruno de atún",
+    price: 4.5,
+    restaurant: { connect: { name: "La Tapeoteca" } },
+    tags: {
+      connect: [
+        { id: "fish" },
+        { id: "mustard" },
+        { id: "gluten" },
+        { id: "nuts" },
+      ],
+    },
+  },
+  {
+    name: "Empanadilla fea de pato, peras y pasas",
+    price: 3.5,
+    restaurant: { connect: { name: "La Tapeoteca" } },
+    tags: {
+      connect: [{ id: "dioxide" }, { id: "gluten" }, { id: "nuts" }],
+    },
+  },
+  {
+    name: "Patata asada",
+    price: 0.6,
+    restaurant: { connect: { name: "Los Zagales" } },
+  },
+  {
+    name: "Caballito",
+    price: 0.6,
+    restaurant: { connect: { name: "Los Zagales" } },
+    tags: {
+      connect: [{ id: "gluten" }],
+    },
+  },
+  {
+    name: "Lomo plancha con queso curado",
+    price: 3.8,
+    restaurant: { connect: { name: "Los Zagales" } },
+    tags: {
+      connect: [{ id: "dairy" }],
+    },
+  },
+];
+
 async function main() {
   console.log(`Start seeding ...`);
   for (const u of userData) {
@@ -18,6 +128,27 @@ async function main() {
       data: u,
     });
     console.log(`Created user with id: ${user.id}`);
+  }
+
+  for (const u of tagData) {
+    const tag = await prisma.tag.create({
+      data: u,
+    });
+    console.log(`Created tag with id: ${tag.id}`);
+  }
+
+  for (const u of restaurantData) {
+    const restaurant = await prisma.restaurant.create({
+      data: u,
+    });
+    console.log(`Created restaurant with id: ${restaurant.id}`);
+  }
+
+  for (const u of dishData) {
+    const dish = await prisma.dish.create({
+      data: u,
+    });
+    console.log(`Created dish with id: ${dish.id}`);
   }
 
   console.log(`Seeding finished.`);
