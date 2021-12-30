77eb273f925e1304cb9be703141f43cdf890f7fd
mikemajara <fmiguel05@gmail.com>
2021-12-30 12:02:59 +0100

    chore: fix image restaurants

--- src/components/cards/restaurant-card.tsx
+++ src/components/cards/restaurant-card.tsx
@@ -15,7 +15,7 @@ import _ from "lodash";
 import next from "next";
 import { RestaurantInclude } from "prisma/model";
 
-const IMAGE_LOCATION = "/images";
+const IMAGE_LOCATION = "/images/restaurants";
 
 export const RestaurantCard = (props: RestaurantInclude) => {
   const { id, name, dishes, images } = props;
@@ -38,7 +38,7 @@ export const RestaurantCard = (props: RestaurantInclude) => {
             as={Image}
             boxSize={220}
             fit={"cover"}
-            src={`${IMAGE_LOCATION}${images?.[0].fileName}`}
+            src={`${IMAGE_LOCATION}/${images?.[0].fileName}`}
             borderRadius={"md"}
             border=".25px solid"
             borderColor="gray.900"
